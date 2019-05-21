57d95ddf9b4f24e59f36fc69628762316a0a33b6
rivani <ivaniuk.r.i@gmail.com>
2019-05-21 12:16:14 +0300

    fixed build

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -10,6 +10,9 @@ until mysql -h "$db_url" -u$db_user -p$db_password -D$db_name -e'show tables;'; 
   sleep 1
 done
 
+sed -i "s/fixJavaKerberos();/fixJavaKerberos();Logger.info(\"Fixed cerberos\");/g" dr-elephant/app/Global.java
+sed -i "s/_drElephantThread = new Thread(DrElephant.getInstance());/_drElephantThread = new Thread(DrElephant.getInstance());Logger.info(\"Got instance.\");/g" dr-elephant/app/Global.java
+sed -i "s/_drElephantThread.start();/_drElephantThread.start();Logger.info(\"Started.\");/g" dr-elephant/app/Global.java
 
 #db.default.url="jdbc:mysql://localhost/playdb"
 #db.default.username=playdbuser
